843f01406bb6d442390de720572df09b7931b095
Jin <jinnkimm7@gmail.com>
2023-08-22 16:26:13 +0900

    Update: Container 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import Container from "@/components/Container";
 import PostList from "@/components/PostList";
 import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Header />
       <PostList />
-    </>
+    </Container>
   );
 }
