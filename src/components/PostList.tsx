8d7f4bdfee1061708bb49903b847ae7191efa225
Jin <jinnkimm7@gmail.com>
2023-08-22 16:25:43 +0900

    Refactor: contentlayer의 allpost로 렌더링

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { compareDesc } from "date-fns";
 import { allPosts } from "@/contentlayer/generated";
 
 function PostList() {
-  const {
-    props: { posts },
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  } = getStaticProps();
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
+  );
 
   return (
     <main>
-      <div className="mb-5">{posts.length} posts</div>
+      <div className="my-5">{posts.length} posts</div>
       <div className="grid grid-cols-2 gap-10">
         {posts.map(({ _id, title, description, category, createdAt }) => (
           <article
@@ -25,7 +25,7 @@ function PostList() {
             </div>
             <div className="flex-grow flex flex-col">
               <Link
-                href={`posts/${_id}`}
+                href={`posts/${_id.replace(/\.md$/, "")}`}
                 className="text-gray-900 text-xl font-bold mb-2 truncate-2-lines"
               >
                 {title}
@@ -38,17 +38,5 @@ function PostList() {
     </main>
   );
 }
-
-export const getStaticProps = () => {
-  const posts = allPosts.sort(
-    (a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt)),
-  );
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
 
 export default PostList;
