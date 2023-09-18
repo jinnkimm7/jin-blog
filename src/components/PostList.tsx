3b11f007f97ea244537b50835889f2b9d328e83d
Jin <jinnkimm7@gmail.com>
2023-09-18 22:47:27 +0900

    Update: tag 버튼 클릭 시 관련 post filter

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,27 +1,44 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import { compareDesc } from "date-fns";
 import { allPosts } from "@/contentlayer/generated";
 
 function PostList() {
+  const [selectedTag, setSelectedTag] = useState("");
+
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
   );
 
+  // 선택된 태그에 해당하는 포스트만 필터링
+  const filteredPosts = selectedTag
+    ? posts.filter(post => post.tags?.includes(selectedTag))
+    : posts;
+
   return (
     <main>
-      <div className="my-5">{posts.length} posts</div>
+      <div className="my-5">{filteredPosts.length} posts</div>
       <div className="grid grid-cols-2 gap-10">
-        {posts.map(({ _id, title, description, category, createdAt }) => (
+        {filteredPosts.map(({ _id, title, description, tags, createdAt }) => (
           <article
             key={_id}
             className="flex flex-col bg-white rounded-lg p-8 h-[230px] hover:shadow-md hover:transform hover:-translate-y-1 transition-transform duration-300"
           >
             <div className="flex justify-between items-center mb-2">
               <span className="text-xs">{createdAt}</span>
-              <span className="bg-gray-100 px-2 rounded-xl text-blue-700">
-                {category}
-              </span>
+              <div>
+                {tags?.map(tag => (
+                  <button
+                    type="button"
+                    className="bg-gray-100 px-2 rounded-xl text-blue-700 ml-2"
+                    onClick={() => setSelectedTag(tag)} // 버튼을 클릭하면 선택된 태그로 설정
+                  >
+                    {tag}
+                  </button>
+                ))}
+              </div>
             </div>
             <Link
               href={`posts/${_id.replace(/\.md$/, "")}`}
